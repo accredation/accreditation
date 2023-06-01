6bced73d10ee1a1835db16bc7f90de64965e6764
Hancharou.Uladzislau <hancharou@rncpmt.by>
2023-06-01 17:36:09 +0300

    профиль

--- index.php
+++ index.php
@@ -68,27 +68,27 @@
                   <img class="img-avatar img-avatar48 img-avatar-thumb" src="assets/images/faces/face28.png" alt="">
                 </div>
                 <div class="p-2">
-                  <h5 class="dropdown-header text-uppercase pl-2 text-dark">User Options</h5>
+                    <?php
+                    if (isset($_COOKIE['login'])) {?>
+                  <h5 class="dropdown-header text-uppercase pl-2 text-dark">Пользовательские данные</h5>
                   <a class="dropdown-item py-1 d-flex align-items-center justify-content-between" href="#">
-                    <span>Inbox</span>
+                    <span>Входящие</span>
                     <span class="p-0">
                       <span class="badge badge-primary">3</span>
                       <i class="mdi mdi-email-open-outline ml-1"></i>
                     </span>
                   </a>
                   <a class="dropdown-item py-1 d-flex align-items-center justify-content-between" href="#">
-                    <span>Profile</span>
+                    <span>Профиль</span>
                     <span class="p-0">
                       <span class="badge badge-success">1</span>
                       <i class="mdi mdi-account-outline ml-1"></i>
                     </span>
                   </a>
-                  <a class="dropdown-item py-1 d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                    <span>Settings</span>
-                    <i class="mdi mdi-settings"></i>
-                  </a>
+
                   <div role="separator" class="dropdown-divider"></div>
-                  <h5 class="dropdown-header text-uppercase  pl-2 text-dark mt-2">Actions</h5>
+                    <?php }?>
+                  <h5 class="dropdown-header text-uppercase  pl-2 text-dark mt-2">Действия</h5>
                     <?php
                     if (isset($_COOKIE['login'])) {?>
                     <a class="dropdown-item py-1 d-flex align-items-center justify-content-between" href="/index.php?logout">
