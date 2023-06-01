ddef07141d03be81d3ff9f0a926b125776ac5034
Hancharou.Uladzislau <hancharou@rncpmt.by>
2023-06-01 17:18:30 +0300

    пользователи

--- elements/navmenu/navmenu.php
+++ elements/navmenu/navmenu.php
@@ -1,3 +1,17 @@
+<?php
+
+$array_categ = [];
+$array_names_categ = [];
+
+if (isset($_SESSION['id_user'])) {
+
+//$query = "SELECT * FROM view_categ_user, category, users WHERE users.id_user = view_categ_user.id_user AND category.id_category = view_categ_user.id_categ AND users.id_user = '{$_SESSION['id_user']}'";
+    $query = "SELECT id_role FROM users WHERE users.id_user = '{$_SESSION['id_user']}'";
+
+    $result = mysqli_query($con, $query) or die (mysqli_error($con));
+
+}
+?>
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
           <ul class="nav">
             <li class="nav-item nav-category"></li>
@@ -27,6 +41,20 @@
                 <span class="menu-title">Помощь</span>
               </a>
             </li>
-
+              <?php
+              if (mysqli_num_rows($result) == 1) //если получена одна строка
+              {
+                $row = mysqli_fetch_assoc($result);
+                if($row['id_role'] == 2){?>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">
+                      <span class="icon-bg"><i class="mdi mdi-contacts menu-icon"></i></span>
+                      <span class="menu-title">Пользователи</span>
+                  </a>
+              </li>
+              <?php
+                }
+              }
+              ?>
           </ul>
         </nav>
