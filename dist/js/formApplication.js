0a6a8c2ebe6f8f16c4d4f84c65de909cf40112ed
Hancharou.Uladzislau <hancharou@rncpmt.by>
2023-06-30 16:42:58 +0300

    заполнение таблиц критериев

--- dist/js/formApplication.js
+++ dist/js/formApplication.js
@@ -140,19 +140,12 @@ function showTab(element,id_sub){
                     divFormGroup.appendChild(divFormCheck);
                     row.appendChild(divFormGroup);
                 }
+                createAccordionCards(id_sub);
             });
 
     }
 
-    return new Promise((resolve, reject) => {
-        console.log("Готово.");
-        // Успех в половине случаев.
-        if (Math.random() > .5) {
-            resolve("Успех")
-        } else {
-            reject("Ошибка")
-        }
-    })
+
 }
 
 function getCookie(cname) {
@@ -368,6 +361,7 @@ function getTabs(name, id_sub){
 
     let cardRight = document.createElement("div");
     cardRight.className = "card";
+    cardRight.id = "cardRight";
     cardRight.style = "margin-left: 15px; width: 65%";
     cardRight.innerHTML = "подзразделения";
 
@@ -430,6 +424,7 @@ function getMainTab(name, id_sub){
 
     let cardRight = document.createElement("div");
     cardRight.className = "card";
+    cardRight.id = "cardRight";
     cardRight.style = "margin-left: 15px; width: 65%";
     cardRight.innerHTML = "главная";
 
@@ -564,23 +559,139 @@ function saveTab(id_sub){
 
 
 function createAccordionCards(id_sub) {
+
+
     let thisTab = document.getElementById("tab" + id_sub + "-");
+    let cardRight = thisTab.querySelector("#cardRight");
+    cardRight.innerHTML = "";
     let arrCheckInputs = thisTab.getElementsByClassName("form-check-input");
     let arrIdCriterias = new Array();
 
-
+    let divAccordion = document.createElement("div");
+    divAccordion.className = "accordion";
+    let divCard = document.createElement("div");
+    divCard.className = "card";
     for (let input of arrCheckInputs) {
-        let id_criteria = input.id.substring(8);
-
 
         if (input.checked === true) {
-            let item = {
-                id_criteria: id_criteria,
-                name_criteria: document.querySelector("label[for='checkbox"+id_criteria+"']")
+            let id_criteria = input.id.substring(8);
+            let name_criteria = document.querySelector("label[for='checkbox"+id_criteria+"']")
 
-            };
-            console.log(document.querySelector("label[for='checkbox"+id_criteria+"']").innerText);
-            arrIdCriterias.push(item);
+
+            let divCardHeader = document.createElement("div");
+            divCardHeader.className = "card-header";
+            divCardHeader.id = "heading" + id_criteria;
+            divCardHeader.style = "justify-content: center; display: flex;";
+            let btnCollapse = document.createElement("button");
+            btnCollapse.className = "btn btn-link";
+            btnCollapse.setAttribute("data-toggle", "collapse");
+            btnCollapse.setAttribute("data-target", "#collapse"+id_criteria);
+            btnCollapse.setAttribute("aria-expanded", "false");
+            btnCollapse.setAttribute("aria-controls", "collapse"+id_criteria);
+            btnCollapse.innerHTML = name_criteria.innerHTML;
+            btnCollapse.style = "text-decoration: none; color: black; font-size: 0.9rem;";
+
+
+
+            divCardHeader.appendChild(btnCollapse);
+            divCard.appendChild(divCardHeader);
+
+            let divCollapse = document.createElement("div");
+            divCollapse.className = "collapse";
+            divCollapse.id = "collapse"+id_criteria;
+            divCollapse.setAttribute("aria-labelledby", "heading"+id_criteria);
+            divCollapse.setAttribute("data-parent", "#accordion");
+            let divCardBody = document.createElement("div");
+            divCardBody.className = "card-body";
+            divCardBody.id = "cardBody"+id_criteria;
+
+            divCollapse.appendChild(divCardBody);
+            divCard.appendChild(divCollapse);
+
+
+            divCardHeader.onclick=() =>{
+                divCollapse.classList.toggle("show");
+
+                let aa = btnCollapse.getAttribute('aria-expanded');
+                if(aa == 'false') {
+                    btnCollapse.setAttribute('aria-expanded',true);
+                    collapseTable(id_criteria, divCardBody,id_sub);
+                } else {
+                    btnCollapse.setAttribute('aria-expanded',false)
+                }
+
+
+
+            }
         }
     }
+
+
+    divAccordion.appendChild(divCard);
+    cardRight.appendChild(divAccordion);
+
 }
+
+function collapseTable(id_criteria, divCardBody,id_sub){
+    divCardBody.innerHTML = "";
+
+
+    $.ajax({
+        url: "getMark.php",
+        method: "GET",
+        data: {id_sub: id_sub, id_criteria: id_criteria}
+    })
+        .done(function( response ) {
+            let marks = JSON.parse(response);
+
+            marks.map((item, index) => {
+                let newRow = document.createElement('div');
+                newRow.className = 'row';
+
+                if (index ==0) {
+                    let newHead1 = document.createElement('div');
+                    newHead1.className = 'col';
+                    newHead1.innerHTML = 'Критерий';
+
+
+                    let newHead2 = document.createElement('div');
+                    newHead2.className = 'col';
+                    newHead2.innerHTML = 'Класс критерия';
+
+                    let newHead3 = document.createElement('div');
+                    newHead3.className = 'col';
+                    newHead3.innerHTML = 'поле 4';
+
+                    let newHeadRow = document.createElement('div');
+                    newHeadRow.className = 'row';
+
+                    newHeadRow.appendChild(newHead1);
+                    newHeadRow.appendChild(newHead2);
+                    newHeadRow.appendChild(newHead3);
+
+
+
+                    divCardBody.appendChild(newHeadRow);
+                }
+
+                let newCol1 = document.createElement('div');
+                newCol1.className = 'col';
+                newCol1.innerHTML = item['mark_name'];
+                let newCol2 = document.createElement('div');
+                newCol2.className = 'col';
+                newCol2.innerHTML = item['mark_class'];
+                let newCol3 = document.createElement('div');
+                newCol3.className = 'col';
+                newCol3.innerHTML = item['id_mark'];
+
+                newRow.appendChild(newCol1);
+                newRow.appendChild(newCol2);
+                newRow.appendChild(newCol3);
+
+                divCardBody.appendChild(newRow);
+
+            });
+        });
+}
+
+
