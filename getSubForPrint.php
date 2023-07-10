3311ef4648f1f95c2d6710e1565fdee5baacdad6
Hancharou.Uladzislau <hancharou@rncpmt.by>
2023-07-10 17:53:21 +0300

    печать

---
+++ getSubForPrint.php
@@ -0,0 +1,46 @@
+<?php
+include "connection.php";
+
+$id_sub = $_GET['id_sub'];
+
+$query = "SELECT rc.id_criteria, CONCAT(c.name,' (', con.conditions,')') as name_criteria,
+ IFNULL(m.mark_name,'') as mark_name, IFNULL(m.mark_class,'') as mark_class, 
+ case when IFNULL(mr.field4,'')=1 then 'Да' when IFNULL(mr.field4,'')=2 then 'Нет' when IFNULL(mr.field4,'')=3 then 'Не требуется' else '' end as field4,
+ IFNULL(mr.field5,'') as field5, IFNULL(mr.field6,'') as field6,
+ case when IFNULL(mr.field7,'')=1 then 'Да' when IFNULL(mr.field7,'')=2 then 'Нет' when IFNULL(mr.field7,'')=3 then 'Не требуется' else '' end as field7,
+ IFNULL(mr.field8,'') as field8
+
+FROM rating_criteria rc
+left outer join criteria c on rc.id_criteria=c.id_criteria
+left outer join conditions con on c.conditions_id=con.conditions_id
+left outer join mark m on rc.id_criteria=m.id_criteria 
+left outer join mark_rating mr on m.id_mark=mr.id_mark and mr.id_subvision='$id_sub'
+
+where rc.id_subvision = '$id_sub'";
+
+$rez = mysqli_query($con, $query) or die("Ошибка " . mysqli_error($con));
+$marks = array();
+$report_sub_marks = array();
+for ($data = []; $row = mysqli_fetch_assoc($rez); $data[] = $row);
+
+class ReportSub{
+    public $id_criteria , $name_criteria, $mark_name, $mark_class, $field4, $field5, $field6, $field7, $field8;
+}
+
+foreach ($data as $app) {
+    $obj = array();
+    $mark = new ReportSub();
+    $mark->id_criteria = $app['id_criteria'];
+    $mark->name_criteria = $app['name_criteria'];
+    $mark->mark_name = $app['mark_name'];
+    $mark->mark_class = $app['mark_class'];
+    $mark->field4 = $app['field4'];
+    $mark->field5 = $app['field5'];
+    $mark->field6 = $app['field6'];
+    $mark->field7 = $app['field7'];
+    $mark->field8 = $app['field8'];
+    array_push($report_sub_marks,$mark);
+}
+
+
+echo json_encode($report_sub_marks);
---
+++ saveMarksAccred.php
@@ -0,0 +1,54 @@
+<?php
+include "connection.php";
+$id_sub = $_POST['id_sub'];
+$marks = $_POST['marks'];
+
+//echo $marks;
+
+foreach($marks as $k=>$v){
+
+  //  echo json_encode($v);
+
+    $id_mark = $v['id_mark'];
+    $field7 = (integer)$v['field7'];
+
+    $field8 = $v['field8'];
+    $id_mark_rating = $v['id_mark_rating'];
+
+ //   echo $id_mark_rating;
+
+    if($id_mark_rating != '') {
+            $insertquery = "update mark_rating
+            set id_mark = '$id_mark', field7='$field7', field8='$field8', id_subvision='$id_sub'
+            where id_mark_rating='$id_mark_rating'";
+        } else {
+            $insertquery = "Insert into mark_rating(id_mark, field7, field8, id_subvision)
+                                values ('$id_mark', '$field7', '$field8', '$id_sub')";
+        }
+
+        mysqli_query($con, $insertquery) or die("Ошибка " . mysqli_error($con));
+}
+
+//foreach($marks as $k=>$v){
+//    $id_mark = $v['id_mark'];
+//    $field4 = $v['field4'];
+//    $field5 = $v['field5'];
+//    $field6 = $v['field6'];
+//    $id_mark_rating = $v['id_mark_rating'];
+//
+//    echo $id_mark;
+//    /*
+//        if($id_mark_rating != 0) {
+//            $insertquery = "UPdate into mark_rating
+//            set id_mark = '$id_mark', field4='$field4', field5='$field5', field6='$field6', id_sub='$id_sub'
+//            where id_mark_rating='$id_mark_rating'";
+//        } else {
+//            $insertquery = "Insert into mark_rating(id_mark, field4, field5, field6, id_sub)
+//                                values ('$id_mark','$field4', '$field5', '$field6', '$id_sub')";
+//        }
+//
+//        mysqli_query($con, $insertquery) or die("Ошибка " . mysqli_error($con));
+//    */
+//}
+
+?>
