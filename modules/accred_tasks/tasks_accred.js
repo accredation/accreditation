f01ec7945693cb6d144a74ea79fb6f674c705c24
NikitaSydykov <nkt.sdkv.1996@gmail.com>
2023-10-05 11:38:45 +0300

    colorchange js restore

--- modules/accred_tasks/tasks_accred.js
+++ modules/accred_tasks/tasks_accred.js
@@ -26,7 +26,7 @@ function getCookie(cname) {
     return "";
 }
 
-if(getCookie("expert") === "1") {
+if(getCookie("isMA") === "0") {
     [...btns].forEach(item => {
         item.disabled = true;
     });
@@ -42,8 +42,6 @@ let id_user = getCookie("id_user");
         let td = item.parentElement.parentElement;
         td.classList.add('trColor');
 
-        // td.classList.add('trGreen');
-
         let id = td.classList[1].substring(7,td.classList[1].length)
         let mainTd = document.getElementById(id);
         mainTd.classList.add('tdMainColor');
@@ -51,21 +49,11 @@ let id_user = getCookie("id_user");
     }
 });
 
-document.querySelectorAll("td").forEach( item => {
-    if (item.innerText === "готово ")
-        item.classList.add('trGreen');
-
-});
-
 let dateAccept = document.getElementById("dateAccept");
 let dateComplete = document.getElementById("dateComplete");
 let dateCouncil = document.getElementById("dateCouncil");
 
 function showModal(id_app){
-    let btnSave = document.getElementById("btnSave");
-    if (getCookie("expert") === "1"){
-        btnSave.classList.add("hidden");
-    }
     this.id_app = id_app;
 
     let parent = document.getElementById(`${id_app}`);
@@ -183,79 +171,79 @@ function saveChanges(btn){
     }
 
 
-     // if(dateCouncil.value !== "")
+    // if(dateCouncil.value !== "")
     // if (new Date(dateComplete.value) < new Date(dateCouncil.value)) {
     //     if(dateCouncil.value === "" && dateComplete.value !== "") {
-            $.ajax({
-                url: "modules/accred_tasks/saveTask.php",
-                method: "POST",
-                data: {
-                    id_application: this.id_app,
-                    date_accept: dateAccept.value,
-                    date_complete: dateComplete.value,
-                    date_council: dateCouncil.value,
-                    id_responsible: id_responsible
-                }
-            }).done(function (response) {
-                trId.children[3].innerHTML = predsName;
-                trId.children[4].innerHTML = dateAccept.value;
-                trId.children[5].innerHTML = dateComplete.value;
-                trId.children[6].innerHTML = dateCouncil.value;
+    $.ajax({
+        url: "modules/accred_tasks/saveTask.php",
+        method: "POST",
+        data: {
+            id_application: this.id_app,
+            date_accept: dateAccept.value,
+            date_complete: dateComplete.value,
+            date_council: dateCouncil.value,
+            id_responsible: id_responsible
+        }
+    }).done(function (response) {
+        trId.children[3].innerHTML = predsName;
+        trId.children[4].innerHTML = dateAccept.value;
+        trId.children[5].innerHTML = dateComplete.value;
+        trId.children[6].innerHTML = dateCouncil.value;
 
-                let startDate2 = new Date(); // текущая дата yy mm dd
-                let endDate2 = new Date(); // конечная дата
-                startDate2.setMonth(startDate2.getMonth() - 5);
-                endDate2.setMonth(endDate2.getMonth() + 5);
+        let startDate2 = new Date(); // текущая дата yy mm dd
+        let endDate2 = new Date(); // конечная дата
+        startDate2.setMonth(startDate2.getMonth() - 5);
+        endDate2.setMonth(endDate2.getMonth() + 5);
 
-                let dateAcF = dateAc; //.toLocaleString('ru-RU').slice(0, 10) ;
-                let dateComplF = dateCompl;//.toLocaleDateString();
-                let dateCouncF = dateCounc;//.toLocaleDateString();
+        let dateAcF = dateAc; //.toLocaleString('ru-RU').slice(0, 10) ;
+        let dateComplF = dateCompl;//.toLocaleDateString();
+        let dateCouncF = dateCounc;//.toLocaleDateString();
 
-                // console.log('dateStartF2', typeof dateAcF);
-                // console.log('dateStartF2', dateAcF);
-                //    console.log(new Date(dateAcF.trim())<  new Date(dateStartF.trim()));
-                if (dateAcF === '') {
-                    dateAcF = day;
-                } else {
-                    if (dateAcF.toISOString() < startDate2.toISOString()) {
-                        dateAcF = startDate2
-                    }
-                }
+        // console.log('dateStartF2', typeof dateAcF);
+        // console.log('dateStartF2', dateAcF);
+        //    console.log(new Date(dateAcF.trim())<  new Date(dateStartF.trim()));
+        if (dateAcF === '') {
+            dateAcF = day;
+        } else {
+            if (dateAcF.toISOString() < startDate2.toISOString()) {
+                dateAcF = startDate2
+            }
+        }
 
-                if (dateComplF === '') {
-                    dateComplF = day;
-                } else {
-                    if (dateComplF.toISOString() > endDate2.toISOString()) {
-                        dateComplF = endDate2
-                    }
-                }
+        if (dateComplF === '') {
+            dateComplF = day;
+        } else {
+            if (dateComplF.toISOString() > endDate2.toISOString()) {
+                dateComplF = endDate2
+            }
+        }
 
 
-                if (dateCouncF === '') {
-                    dateCouncF = day;
-                } else {
-                    if (dateCouncF.toISOString() > endDate2.toISOString()) {
-                        dateCouncF = endDate2
-                    }
-                }
+        if (dateCouncF === '') {
+            dateCouncF = day;
+        } else {
+            if (dateCouncF.toISOString() > endDate2.toISOString()) {
+                dateCouncF = endDate2
+            }
+        }
 
-                ulId.children[0].setAttribute("data-duration", dateAcF.toLocaleString('ru-RU').slice(0, 5) + "-" + dateComplF.toLocaleString('ru-RU').slice(0, 5));
-                if (dateCouncil.value === "") {
-                    if (dateCompl === "") {
-                        dateCompl = new Date();
-                    }
-                    ulId.children[1].setAttribute("data-duration", dateComplF.toLocaleString('ru-RU').slice(0, 5) + "-" + dateComplF.toLocaleString('ru-RU').slice(0, 5));
-                    ulId.children[1].style.backgroundColor = "#CE3F31";
-                } else {
-                    ulId.children[1].setAttribute("data-duration", dateCouncF.toLocaleString('ru-RU').slice(0, 5) + "-" + dateCouncF.toLocaleString('ru-RU').slice(0, 5));
-                    ulId.children[1].style.backgroundColor = "#8A231A";
-                }
-                btn.addEventListener("mouseout", createChart);
+        ulId.children[0].setAttribute("data-duration", dateAcF.toLocaleString('ru-RU').slice(0, 5) + "-" + dateComplF.toLocaleString('ru-RU').slice(0, 5));
+        if (dateCouncil.value === "") {
+            if (dateCompl === "") {
+                dateCompl = new Date();
+            }
+            ulId.children[1].setAttribute("data-duration", dateComplF.toLocaleString('ru-RU').slice(0, 5) + "-" + dateComplF.toLocaleString('ru-RU').slice(0, 5));
+            ulId.children[1].style.backgroundColor = "#CE3F31";
+        } else {
+            ulId.children[1].setAttribute("data-duration", dateCouncF.toLocaleString('ru-RU').slice(0, 5) + "-" + dateCouncF.toLocaleString('ru-RU').slice(0, 5));
+            ulId.children[1].style.backgroundColor = "#8A231A";
+        }
+        btn.addEventListener("mouseout", createChart);
 
-                alert("Задача сохранена");
+        alert("Задача сохранена");
 
-                modal.classList.remove("show");
-            });
+        modal.classList.remove("show");
+    });
     //     }
     // }
     // else alert("Дата совета не может быть меньше даты завершения проверки");
@@ -343,21 +331,6 @@ function createChart(e) {
         if (e.type == "load") {
             el.style.backgroundColor = el.dataset.color;
             el.style.opacity = 1;
-
-        }
-
-        let ulZ = el.parentElement;
-        let idApp = ulZ.id.substring(2);
-        if(Number(idApp)) {
-
-            let tdAc = document.getElementById("date_accept_" + idApp);
-            let tdCompl = document.getElementById("date_complete_" + idApp);
-
-            if ((tdCompl.innerText=='' || tdAc.innerText=='')) {
-                el.style.opacity = 0;
-            } else {
-                el.style.opacity = 1;
-            }
         }
     });
 
@@ -372,27 +345,27 @@ window.addEventListener("resize", createChart);
 
 
 function collapsTable(id) {
-        //console.log(id, date_accept, date_complete);
-        let date_accept = document.getElementById('date_accept_'+id);
-        let date_complete = document.getElementById('date_complete_'+id);
+    //console.log(id, date_accept, date_complete);
+    let date_accept = document.getElementById('date_accept_'+id);
+    let date_complete = document.getElementById('date_complete_'+id);
 
-             // console.log(date_accept.innerText)
-        date_accept =  date_accept.innerText;
-        date_complete=  date_complete.innerText;
+    // console.log(date_accept.innerText)
+    date_accept =  date_accept.innerText;
+    date_complete=  date_complete.innerText;
 
-        if(date_accept!=='' && date_complete!==''){
+    if(date_accept!=='' && date_complete!==''){
 
 
 
-            let myUl = document.getElementById("ul" + id);
-            if (myUl.classList.contains("hidden")) {
-                // myUl.classList.remove("hidden");
-                // myUl.classList.add("visib");
-            } else {
-                // myUl.classList.remove("visib");
-                // myUl.classList.add("hidden");
-            }
+        let myUl = document.getElementById("ul" + id);
+        if (myUl.classList.contains("hidden")) {
+            // myUl.classList.remove("hidden");
+            // myUl.classList.add("visib");
+        } else {
+            // myUl.classList.remove("visib");
+            // myUl.classList.add("hidden");
         }
+    }
 
 
     let parent = document.getElementById(`${id}`);
@@ -403,56 +376,56 @@ function collapsTable(id) {
     let  filteredHidden = arrHiden.filter((item) => item.classList.contains("fill_sub") === false)
 
     if(getCookie("isMA") === "1") {
-    let arrOtv = new Array();
-    $.ajax({
-        url: "modules/accred_tasks/getDoctorExpert.php",
-        method: "GET",
-        data: {id_application: id}
-    }).done(function (response){
-        for (let i of JSON.parse(response)){
-            arrOtv.push(i);
-        }
+        let arrOtv = new Array();
+        $.ajax({
+            url: "modules/accred_tasks/getDoctorExpert.php",
+            method: "GET",
+            data: {id_application: id}
+        }).done(function (response){
+            for (let i of JSON.parse(response)){
+                arrOtv.push(i);
+            }
 
 
-        if (arrOtv.length > 0) {
-            filteredHidden.forEach((hidenTrItem) => {
+            if (arrOtv.length > 0) {
+                filteredHidden.forEach((hidenTrItem) => {
 
-                let idCr = hidenTrItem.children[0].id.substring(2);
+                    let idCr = hidenTrItem.children[0].id.substring(2);
 
-                let select = hidenTrItem.getElementsByTagName('select')[0];
-                let selInd = select.value;
+                    let select = hidenTrItem.getElementsByTagName('select')[0];
+                    let selInd = select.value;
 
 
-                let opt = [...select.options];
+                    let opt = [...select.options];
 
-                let firstOption = document.createElement('option');
-                firstOption.id = "otv" + 0;
-                firstOption.value = "0";
-                firstOption.text = "";
+                    let firstOption = document.createElement('option');
+                    firstOption.id = "otv" + 0;
+                    firstOption.value = "0";
+                    firstOption.text = "";
 
-                let filtDoctor = arrOtv.filter((item) => item['id_criteria'] == idCr);
-                let newarr = [...filtDoctor];
+                    let filtDoctor = arrOtv.filter((item) => item['id_criteria'] == idCr);
+                    let newarr = [...filtDoctor];
 
-                opt.map(optItem=> {
-                    newarr = newarr.filter((item) => item['id_user'] != optItem.value);
-                })
-
-                if (newarr.length > 0) {
-                    newarr.map(item => {
-                        let newOption = document.createElement('option');
-                        newOption.id = "otv" + item['id_user'];
-                        newOption.value = item['id_user'];
-                        newOption.text = item['username'];
-                        select.appendChild(newOption);
+                    opt.map(optItem=> {
+                        newarr = newarr.filter((item) => item['id_user'] != optItem.value);
                     })
-                }
-                select.value = selInd;
 
-            });
+                    if (newarr.length > 0) {
+                        newarr.map(item => {
+                            let newOption = document.createElement('option');
+                            newOption.id = "otv" + item['id_user'];
+                            newOption.value = item['id_user'];
+                            newOption.text = item['username'];
+                            select.appendChild(newOption);
+                        })
+                    }
+                    select.value = selInd;
 
-        }
+                });
 
-    });
+            }
+
+        });
 
     }
 
@@ -518,9 +491,7 @@ let ulhline = document.getElementById("nowDate");
 let hline = ulhline.getElementsByTagName("li")[0];
 
 let mytask = document.getElementById("mytask");
-let qq1 = document.getElementsByClassName("chart-wrapper")[0];
-let clH1 = qq1.clientHeight;
-hline.style.height = clH1 + "px";/*(mytask.offsetHeight -90).toString() + 'px';*/
+hline.style.height = (mytask.offsetHeight -90).toString() + 'px';
 
 
 //скрипт формируем даты
@@ -541,15 +512,11 @@ while (currentDate <= endDate) {
     li.textContent = date;
     li.id = date; // добавляем id
     let li1 = document.createElement("li");
-    const options = { weekday: 'long' };
-    const dayOfWeek = startDate.toLocaleString('en-US', options);
 
-    if(dayOfWeek === "Saturday" || dayOfWeek === "Sunday"){
+    if(dayZ % 2 === 0){
         li.style = "background-color: rgb(227 227 227); height: 100%";
         li1.setAttribute("data-duration",date+"-"+date);
-        let qq = document.getElementsByClassName("chart-wrapper")[0];
-        let clH = qq.clientHeight;
-        li1.style = "margin: 0px; position: absolute; padding: 1px; height:"+ clH + "px; z-index: 1; background-color: rgb(227 227 227); opacity: 1; min-width: 50px; border-radius: 0"
+        li1.style = "margin: 0px; position: absolute; padding: 1px; height: 317px; z-index: 1; background-color: rgb(227 227 227); opacity: 1; min-width: 50px; border-radius: 0"
         document.querySelector('.chart-bars').appendChild(li1);
     }
     dayZ++;
@@ -559,6 +526,8 @@ while (currentDate <= endDate) {
     // увеличиваем тек дату на 1
     currentDate.setDate(currentDate.getDate() + 1);
 }
+
+
 
 
 
@@ -596,15 +565,21 @@ window.onload = () => {
         })
         res = ( lrs/ rrs) ;
         if(Number(res)){
-            document.getElementById(`${id}`).children[7].innerHTML = (res * 100).toFixed(2) +'%' ;
+            document.getElementById(`${id}`).children[8].innerHTML = (res * 100).toFixed(2) +'%' ;
             document.getElementById("ul"+`${id}`).children[0].innerHTML += " Прогресс: " + (res * 100).toFixed(2) +'%';
 
         } else {
-            document.getElementById(`${id}`).children[7].innerHTML = '0' +'%';
+            document.getElementById(`${id}`).children[8].innerHTML = '0' +'%';
         }
 
     });
 }
+
+
+
+
+
+
 function changeOtv(el){
     let id = el.id.substring(2);
     let id_userotv = el.options[el.selectedIndex].value;
@@ -652,69 +627,8 @@ function stopDrag() {
     document.removeEventListener("mouseup", stopDrag);
 }
 
-function printReprot() {
-    let tableMain = document.getElementById('table');
-
-    let table = tableMain.cloneNode(true)
-
-    let thead = table.getElementsByTagName('thead')[0]
-    let trHead = thead.getElementsByTagName('tr')[0]
-    let th = trHead.getElementsByTagName('th')[trHead.getElementsByTagName('th').length-1]
-    th.setAttribute('hidden', 'true')
-
-    let tbody = table.getElementsByTagName('tbody')[0]
-    let trBody = tbody.getElementsByTagName('tr')
-    
-    for (let item of trBody){
-        if(item.classList.contains('question')){
-            item.style = 'font-weight: 700';
-            let tdq = item.getElementsByTagName('td')[item.getElementsByTagName('td').length-1]
-            tdq.setAttribute('hidden', 'true')
-        }
-
-        let tdq = item.getElementsByTagName('td')
-        for(let tdItem of tdq){
-          if(tdItem.getElementsByTagName('select')[0]){
-            let div = document.createElement('div');
-            div.innerHTML = tdItem.getElementsByTagName('select')[0].textContent;
-            let select = tdItem.getElementsByTagName('select')[0];
-            select.setAttribute('hidden', 'true');
-          }
-        }
-
-        if(item.classList.contains('fill_sub')){
-            
-            item.style = 'font-style: italic;'
-           // console.log(style)
-        }
-
-
-    }
-
- 
-    var WinPrint = window.open('','','left=50,top=50,width=800,height=640,toolbar=0,scrollbars=1,status=0');
-
-    WinPrint.document.write('<style>@page {\n' +
-        'size: A4 landscape;\n' +
-        'margin: 1rem;\n' +
-        '}</style>');
-        
-
-    let divReportTitle = document.createElement('div');
-    divReportTitle.innerHTML = 'График работы медицинской аккредитации'
-    divReportTitle.style = 'text-align: center; font-size: 18px; margin-bottom: 2rem; font-weight:700'
-
-    WinPrint.document.write(divReportTitle.outerHTML);
-    WinPrint.document.write(table.outerHTML);
-   
-    WinPrint.document.close();
-    WinPrint.focus();
-    WinPrint.print();
-    WinPrint.close();
-
-    
-}
-
+//header.addEventListener("mousedown", startDrag);
+//ззз
 function handleColorChange(colorPicker, appId) {
     let ulId = document.getElementById("ul"+this.id_app);
     const selectedColor = colorPicker.value;
@@ -738,4 +652,43 @@ function handleColorChange(colorPicker, appId) {
 }
 
 
-//header.addEventListener("mousedown", startDrag);
+
+
+//изменить цвет при загрузке + цвет колонки
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const colors = JSON.parse(xhr.responseText);
+            colors.forEach((colorData) => {
+                const { appId, color } = colorData;
+
+                const targetSelect = document.querySelector(`.question[id='${appId}'] .color-picker`);
+                const selectedItem = targetSelect.querySelector(`option[value='${color}']`);
+
+                if (selectedItem) {
+                    selectedItem.selected = true;
+                }
+
+                const targetChartBar = document.querySelector(`#ul${appId} li:nth-child(2)[data-duration]`);
+                if (targetChartBar) {
+                    targetChartBar.setAttribute('data-color', color);
+                    targetChartBar.style.backgroundColor = color;
+                }
+
+                const targetTd = document.querySelector(`.question[id='${appId}'] td:first-child`);
+                if (targetTd) {
+                    targetTd.style.backgroundColor = color;
+                }
+            });
+        }
+    };
+
+    xhr.open('GET', 'modules/accred_tasks/load_colors.php', true);
+    xhr.send(null);
+
+
+});
