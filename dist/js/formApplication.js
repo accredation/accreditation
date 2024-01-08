fbfab024ec4d3f837e9f25cd128a60ce3cc679f1
NikitaSydykov <nkt.sdkv.1996@gmail.com>
2024-01-08 14:18:44 +0300

    13-14

--- dist/js/formApplication.js
+++ dist/js/formApplication.js
@@ -1242,8 +1242,8 @@ function createTableForPrintNo(tableForPrint) {
             td4.style = "border: 1px solid black; padding: 0.2rem 0.75rem";
             let myArr = item['field5'].split(';');
             myArr.forEach(myitem => {
-                td4.innerHTML += myitem;
-            })
+                td4.innerHTML += myitem + "<br>";
+            });
 
             let td5 = document.createElement('td');
             td5.style = "border: 1px solid black; padding: 0.2rem 0.75rem";
