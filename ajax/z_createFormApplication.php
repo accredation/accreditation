7d96fc84879b76984fcc99bc3210bf9ff7c6c6a2
Hancharou.Uladzislau <hancharou@rncpmt.by>
2024-01-16 10:38:34 +0300

    final2 Форма заявления

--- ajax/z_createFormApplication.php
+++ ajax/z_createFormApplication.php
@@ -3,11 +3,12 @@ include "connection.php";
 $id_app = $_GET['id_application'];
 
 
-
+//echo '<tr><td colspan="4" style="font-weight: 700; text-align: center">' . $name_sub . '</td></tr>';
 echo '
 <style>
 body{
-
+margin-top:30px;
+margin-right: -80px;
 font-family:  \'Times New Roman\' ;
 }
 table {
@@ -16,9 +17,10 @@ table {
 }
 
 th, td {
-  border: 1px solid #ddd;
+   border: 1px solid black;
   padding: 8px;
   text-align: center;
+  font-size: 19pt;
 }
 td {
   word-break: break-word;
@@ -37,67 +39,70 @@ tr:hover {
 
 </style>
 
-      <div style="font-size: 14pt;
+      <div style="font-size: 20pt;
     position: relative;
-    left: 75%;
+    left: 60%;
     
 ">Директору РНПЦ МТ</div>
-      <div style=" font-size: 14pt;
+      <div style=" font-size: 20pt;
     position: relative;
-    left: 75%;
+    left: 60%;
 ">Щербинскому А.А.</div>
 <br><br><br>
-      <div style=" font-size: 14pt;
+      <div style=" font-size: 20pt;
     position: relative;
     text-align: center;
 ">ЗАЯВЛЕНИЕ</div>
-      <div style=" font-size: 15pt;
+      <div style=" font-size: 20pt;
     position: relative;
     text-align: center;
 ">о получении свидетельства о соответствии государственной</div>
-      <div style=" font-size: 15pt;
+      <div style=" font-size: 20pt;
     position: relative;
     text-align: center;
 ">организации здравоохранения базовым критериям медицинской</div>
-      <div style=" font-size: 15pt;
+      <div style=" font-size: 20pt;
     position: relative;
    text-align: center;
 ">аккредитации, внесении в него изменений</div>
-      <div style="font-size: 14pt;
+      <div style="font-size: 20pt;
     position: relative;
     left: 10%;
 ">
 <br>
-Прошу выдать____________________________________________________________________________________</div>
+Прошу выдать_____________________________________________________________</div>
       <div style="font-style: italic;
     position: relative;
     left: 35%;
-    font-size: 9pt;
+    font-size: 14pt;
 ">(наименование заинтересованного лица,</div>
       <div style="
     position: relative;
     left: 10%;
-">________________________________________________________________________________________________________________</div>
+    font-size: 20pt;
+">_________________________________________________________________________</div>
       <div style="font-style: italic;
     position: relative;
     left: 34%;
-    font-size: 9pt;
+    font-size: 14pt;
 ">его местонахождения, юридический адрес,</div>
       <div style="
     position: relative;
     left: 10%;
-">________________________________________________________________________________________________________________</div>
+    font-size: 20pt;
+">_________________________________________________________________________</div>
       <div style="font-style: italic;
     position: relative;
     left: 30%;
-    font-size: 9pt;
-">контактная информация (телефон(факс), электронный адрес)</div>
+    font-size: 14pt;
+">контактная информация (телефон (факс), электронный адрес)</div>
       <div style="
     position: relative;
     left: 10%;
-">________________________________________________________________________________________________________________</div>
+    font-size: 20pt;
+">_________________________________________________________________________</div>
 <br>
-       <div style="width: 80%; font-size: 15pt;
+       <div style="width: 80%; font-size: 20pt;
     position: relative;
     left: 10%; text-align: justify;
 ">свидетельство о соответствии государственной организации здравоохранения базовым критериям медицинской аккредитации по видам оказываемой медицинской помощи по профилям заболеваний, состояний, синдромов, методов их исследования:</div>
@@ -107,16 +112,16 @@ tr:hover {
  <div style="display: flex; justify-content: center; margin-top: 20px">
 <table style="border: 1px solid black; width: 80%;">
     <tr>
-        <th  style=" text-align: center; width: 3%">
+        <th  style=" text-align: center; width: 5%">
             № п/п
         </th>
         <td style=" width: 25%; text-align: center;">
             Наименование вида медицинской помощи
         </td>
-        <td style="  width: 25%; text-align: left;">
+        <td style="  width: 25%; text-align: center;">
             Наименование профиля заболеваний, состояния, синдрома
         </td>
-        <td style="width: 25%;    word-break: break-word;">
+        <td style="width: 25%;    word-break: break-word; text-align: center;">
             Наименование подразделения
         </td>
         </tr><tbody>';
@@ -128,8 +133,7 @@ $ind = 1;
 while ($row_subs = mysqli_fetch_assoc($result_subs)) {
     $name_sub = $row_subs["name"];
     $id_sub = $row_subs["id_subvision"];
-    echo '
-    <tr><td colspan="4" style="font-weight: 700; text-align: center">' . $name_sub . '</td></tr>';
+
     $query_tds = "select tt.name as namet, ltc.name_profile as namel, dep.name as named FROM z_department AS dep
                         JOIN z_list_tables_criteria AS ltc ON ltc.id_list_tables_criteria = dep.id_list_tables_criteria  
                         JOIN z_types_tables AS tt ON tt.id_types_tables = ltc.id_types_tables
@@ -164,46 +168,46 @@ else{
 echo '</br></br>
 <div style="
     position: relative;
-    left: 10%;font-size: 14pt;
+    left: 10%;font-size: 19pt;
 ">Руководитель (представитель)</div>
 <div style="
     position: relative;
-    left: 10%;font-size: 14pt;
+    left: 10%;font-size: 19pt;
 ">заинтересованного лица: </div>
 <br>
 <br>
 <div style="
     position: relative;
     left: 10%;
-">___________________________
+">___________________________________
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;
-____________
+&nbsp;&nbsp;&nbsp;
+
+________________________
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;
 
-____________________</div>
+
+__________________________</div>
 <div class = "podpiska" style="display: flex;">
  <div style="
     position: relative;
     left: 10%;
-    font-size: 9pt;
+    font-size: 14pt;
 ">(наименование должности служащего)</div>
  <div style="
     position: relative;
-    left: 31%;
-    font-size: 9pt;
+    left: 25%;
+    font-size: 14pt;
 ">(подпись)</div>
  <div style="
     position: relative;
-    left: 54%;
-    font-size: 9pt;
+    left: 43%;
+    font-size: 14pt;
 ">(инициалы,фамилия)</div>
 </div>
 ';
