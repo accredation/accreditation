9ed79570d0e4ca1b7530a48b980f5a49ceaead24
Hancharou.Uladzislau <hancharou@rncpmt.by>
2024-01-18 14:45:44 +0300

    улучшенная загрузка из csv в таблицу критериев

--- csvToTableR1.php
+++ csvToTableR1.php
@@ -2,14 +2,29 @@
 <?php
 include "ajax/connection.php";
 
+$input_file = fopen("csv/r2.txt", "r");
+$output_file = fopen("csv/r2.csv", "w");
+
+if ($input_file && $output_file) {
+    while (($line = fgets($input_file)) !== false) {
+        $line = preg_replace('/\s+/', ';', $line, 1);
+        fwrite($output_file, $line);
+    }
+    fclose($input_file);
+    fclose($output_file);
+
+} else {
+    echo "хуй";
+}
+
 $row = 1;
-if (($handle = fopen("csv/r1.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1500, ";")) !== FALSE) {
-        $num = count($data);
-        $row++;
+if (($handle = fopen("csv/r2.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1500,';')) !== FALSE) {
+
+        echo $data[0] . "   " . $data[1] . "<br>";
         $insertquery = "INSERT INTO `z_criteria`(`pp`,
                                      `name`, `id_list_tables_criteria`)
-                                     VALUES ('{$data[0]}', '{$data[1]}', '1')";
+                                     VALUES ('{$data[0]}', '{$data[1]}', '2')";
         $result = mysqli_query($con, $insertquery) or die("Ошибка " . mysqli_error($con));
     }
     echo "се ок";
