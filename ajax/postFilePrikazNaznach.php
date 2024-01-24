580a86f5e5b7e08ac1cc99feea3f97333d1ba099
NikitaSydykov <nkt.sdkv.1996@gmail.com>
2024-01-24 15:14:42 +0300

    file prikaz +

---
+++ ajax/postFilePrikazNaznach.php
@@ -0,0 +1,32 @@
+<?php
+include "connection.php";
+$login = $_COOKIE['login'];
+if(!isset($login)){
+    $login = $_COOKIE['login1'];
+}
+$id_application = $_POST['id_application'];
+
+if (!file_exists('../docs/documents/'.$login.'/'.$id_application)) {
+    mkdir('../docs/documents/'.$login.'/'.$id_application, 0777, true);
+}
+
+
+
+if (isset($_FILES['prikazNaznach']['name'])) {
+    $file_name = $_FILES['prikazNaznach']['name'];
+    $file_tmp = $_FILES['prikazNaznach']['tmp_name'];
+
+    move_uploaded_file($file_tmp, "../docs/documents/" . $login.'/'.$id_application . "/" . $file_name);
+
+    $insertquery =
+        "UPDATE applications set prikazNaznach = '$file_name' where id_application='$id_application'";
+
+    $result = mysqli_query($con, $insertquery) or die("Ошибка " . mysqli_error($con));
+}
+
+
+
+
+
+
+?>
