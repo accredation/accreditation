8123606a6d456da9cb92b3937808e068b2f765a3
NikitaSydykov <nkt.sdkv.1996@gmail.com>
2024-08-23 12:39:54 +0300

    21082024 починка документов при загрузке в повторном заявлении

--- ajax/postFileCopyRasp.php
+++ ajax/postFileCopyRasp.php
@@ -5,14 +5,20 @@ if(!isset($login)){
     $login = $_COOKIE['login1'];
 }
 $id_application = $_POST['id_application'];
-
+$new_id_application = $id_application;
+$query = "SELECT * FROM applications WHERE id_application = '$id_application'";
+$result = mysqli_query($con, $query);
+    $row = mysqli_fetch_assoc($result);
+        $selPervtor = $row['sel_pervtor'];
+        $idOldApp = $row['id_old_app'];
+if ($idOldApp !== null && $idOldApp !== '')
+{
+    $id_application = $idOldApp;
+}
 
 if (!file_exists('../docs/documents/'.$login.'/'.$id_application)) {
     mkdir('../docs/documents/'.$login.'/'.$id_application, 0777, true);
 }
-
-
-
 if (isset($_FILES['copyRasp']['name'])) {
     $file_name = $_FILES['copyRasp']['name'];
     $file_tmp = $_FILES['copyRasp']['tmp_name'];
@@ -20,7 +26,7 @@ if (isset($_FILES['copyRasp']['name'])) {
     move_uploaded_file($file_tmp, "../docs/documents/" . $login.'/'.$id_application . "/" . $file_name);
 
     $insertquery =
-        "UPDATE applications set copy_rasp = '$file_name' where id_application='$id_application'";
+        "UPDATE applications set copy_rasp = '$file_name' where id_application='$new_id_application'";
 
     $result = mysqli_query($con, $insertquery) or die("Ошибка " . mysqli_error($con));
 }
--- ajax/postFileTechOsn.php
+++ ajax/postFileTechOsn.php
@@ -5,6 +5,16 @@ if(!isset($login)){
     $login = $_COOKIE['login1'];
 }
 $id_application = $_POST['id_application'];
+$new_id_application = $id_application;
+$query = "SELECT * FROM applications WHERE id_application = '$id_application'";
+$result = mysqli_query($con, $query);
+$row = mysqli_fetch_assoc($result);
+$selPervtor = $row['sel_pervtor'];
+$idOldApp = $row['id_old_app'];
+if ($idOldApp !== null && $idOldApp !== '')
+{
+    $id_application = $idOldApp;
+}
 
 if (!file_exists('../docs/documents/'.$login.'/'.$id_application)) {
     mkdir('../docs/documents/'.$login.'/'.$id_application, 0777, true);
@@ -19,7 +29,7 @@ if (isset($_FILES['techOsn']['name'])) {
     move_uploaded_file($file_tmp, "../docs/documents/" . $login.'/'.$id_application . "/" . $file_name);
 
     $insertquery =
-        "UPDATE applications set tech_osn = '$file_name' where id_application='$id_application'";
+        "UPDATE applications set tech_osn = '$file_name' where id_application='$new_id_application'";
 
     $result = mysqli_query($con, $insertquery) or die("Ошибка " . mysqli_error($con));
 }
